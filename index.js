78921597221596db671175a10b5f27735f8675fc
dkreidler <dkreidler@gmail.com>
2018-08-22 16:25:44 -0400

    Getting started...

---
+++ index.js
@@ -0,0 +1,15 @@
+// create needed constants
+const rememberDiv = document.querySelector('.remember');
+const forgetDiv = document.querySelector('.forget');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+const personalGreeting = document.querySelector('.personal-greeting');
+
+// Stop the form from submitting when a button is pressed
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
